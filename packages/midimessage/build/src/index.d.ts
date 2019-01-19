7c8b4733e4b1f5992f38eb8af14ed9b46156af66
Hugh Rawlinson <hughr2005@gmail.com>
2019-01-19 14:02:12 -0500

    monorepoify

---
+++ packages/midimessage/build/src/index.d.ts
@@ -0,0 +1,26 @@
+declare enum ChannelModeMessage {
+    "ALL_SOUND_OFF" = 120,
+    "RESET_ALL_CONTROLLERS" = 121,
+    "LOCAL_CONTROL" = 122,
+    "ALL_NOTES_OFF" = 123
+}
+export declare function clampToUint7(n: number): uint7;
+export declare type uint7 = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 | 64 | 65 | 66 | 67 | 68 | 69 | 70 | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 | 80 | 81 | 82 | 83 | 84 | 85 | 86 | 87 | 88 | 89 | 90 | 91 | 92 | 93 | 94 | 95 | 96 | 97 | 98 | 99 | 100 | 101 | 102 | 103 | 104 | 105 | 106 | 107 | 108 | 109 | 110 | 111 | 112 | 113 | 114 | 115 | 116 | 117 | 118 | 119 | 120 | 121 | 122 | 123 | 124 | 125 | 126 | 127;
+declare type ControllerNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 | 64 | 65 | 66 | 67 | 68 | 69 | 70 | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 | 80 | 81 | 82 | 83 | 84 | 85 | 86 | 87 | 88 | 89 | 90 | 91 | 92 | 93 | 94 | 95 | 96 | 97 | 98 | 99 | 100 | 101 | 102 | 103 | 104 | 105 | 106 | 107 | 108 | 109 | 110 | 111 | 112 | 113 | 114 | 115 | 116 | 117 | 118 | 119;
+declare type uint4 = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15;
+declare type Channel = uint4;
+export declare type KeyNumber = uint7;
+export declare type Velocity = uint7;
+declare type PressureValue = uint7;
+declare type ControllerValue = uint7;
+export declare type Duration = number;
+declare type ControllerNumberOrChannelModeMessage = ControllerNumber | ChannelModeMessage;
+declare type MidiByteArray = number[];
+interface MidiMessage {
+    noteOn(k: KeyNumber, v: Velocity): MidiByteArray;
+    noteOff(k: KeyNumber, v: Velocity): MidiByteArray;
+    polyphonicKeyPressure(k: KeyNumber, p: PressureValue): MidiByteArray;
+    controlChange(c: ControllerNumberOrChannelModeMessage, v: ControllerValue): MidiByteArray;
+}
+export declare function withChannel(channel: Channel): MidiMessage;
+export {};
