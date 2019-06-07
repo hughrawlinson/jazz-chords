d1552de5a9803eb99ff1e0fcb5d1a2f9eedd68b1
Hugh Rawlinson <hughr2005@gmail.com>
2019-06-06 20:20:50 -0400

    Some uncheckedin work I found in my working directory. No idea if it works. /shrug

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -7,28 +7,54 @@ import {
   KeyNumber,
   Velocity,
   Duration,
-  clampToUint7
+  clampToUint7,
+  MidiByteArray
 } from '@hughrawlinson/midimessage';
 
 const notes = withChannel(0);
 
+type scoreNote = [KeyNumber, Velocity, Duration]
+type ScoreChord = Array<scoreNote>
+type Score = Array<ScoreChord>
+
+interface NotePlayer {
+  playNote(k: KeyNumber, v: Velocity, d: Duration): void,
+  playChord(s: ScoreChord, n: NotePlayer): void,
+  playSequence(s: Score): void
+}
+
 function withPort(port: WebMidi.MIDIOutput) {
-  return {
-    playNote: (key: KeyNumber, v: Velocity, d: Duration) => {
-      port.send(notes.noteOn(key, v));
-      setTimeout(() => port.send(notes.noteOff(key, 0)), d);
-    }
+  const playNote = (key: KeyNumber, v: Velocity, d: Duration) => {
+    port.send(notes.noteOn(key, v));
+    setTimeout(() => port.send(notes.noteOff(key, 0)), d);
   }
+
+  const playChord = (scoreChord: ScoreChord, notePlayer: NotePlayer) => {
+      scoreChord.map(n => `${n}4`).forEach((note) => {
+        notePlayer.playNote(clampToUint7(Note.midi(note) || 0), 127, 250);
+      });
+  }
+
+  const playSequence = (sequence: Score) => {
+
+  }
+
+  return {
+    playNote,
+    playChord,
+    playSequence
+  } as NotePlayer;
+}
+
+function tonalChordToUsChord(tonalChord: string[]): ScoreChord {
+  return [[tonalChord[0], 2].map(clampToUint7).concat([0])] as ScoreChord;
 }
 
 navigator.requestMIDIAccess().catch(console.log).then(midiAccess => {
   if (midiAccess) {
     midiAccess.outputs.forEach((port, key) => {
       const notePlayer = withPort(port);
-      console.log(Chord.intervals("Asus4"));
-      Chord.notes("Asus4").map(n => `${n}4`).forEach(note => {
-        notePlayer.playNote(clampToUint7(Note.midi(note) || 0), 127, 250);
-      });
+      notePlayer.playChord(tonalChordToUsChord(Chord.notes("Asus4")), notePlayer);
     });
   }
 });
